cac79b52fe464adfc709a6da82b3c222b9c65a5a
Pauline De Ro <pauline.dero@gmail.com>
2021-03-16 19:43:31 +0100

    new repo cowsay initialized

---
+++ index.js
@@ -0,0 +1,10 @@
+const id = require('./information.js');
+id();
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : "Hello, i'm Pauline from Wild Code School BXL",
+	e : "oO",
+	T : "U "
+}));
